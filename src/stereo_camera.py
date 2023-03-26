8ae6cea0bc4363f5baa0832560315115ae038b13
cosheimil <a.varvus@ya.ru>
2023-03-26 18:37:06 +0300

    better white balance

--- src/stereo_camera.py
+++ src/stereo_camera.py
@@ -1,9 +1,10 @@
 import cv2 as cv
 import numpy as np
+
 # from matplotlib import pyplot as plt
 
 
-class PS5_Cam():
+class PS5_Cam:
     width = 1280
     height = 376
     fps = 120
@@ -16,30 +17,27 @@ class PS5_Cam():
         self.video_capture.set(cv.CAP_PROP_FRAME_WIDTH, self.width)
         self.video_capture.set(cv.CAP_PROP_FRAME_HEIGHT, self.height)
         self.video_capture.set(cv.CAP_PROP_FPS, self.fps)
-
+        self.video_capture.set(cv.CAP_PROP_AUTO_WB, 0.0)
+        self.set_wb()
 
     def set_fps(self, fps):
         self.fps = fps
         self.video_capture.set(cv.CAP_PROP_FPS, fps)
 
-    
     def set_height(self, height):
         self.height = height
         self.video_capture.set(cv.CAP_PROP_FRAME_HEIGHT, height)
-
 
     def set_width(self, width):
         self.width = width
         self._delemiter = width // 2
         self.video_capture.set(cv.CAP_PROP_FRAME_WIDTH, width)
 
-
     def read_color(self):
         ret, frame = self.video_capture.read()
-        video_r, video_l = frame[:, :self._delemiter], frame[:, self._delemiter:]
+        video_r, video_l = frame[:, : self._delemiter], frame[:, self._delemiter :]
 
         return video_l, video_r
-    
 
     def read_gray(self):
         video_l, video_r = self.read_color()
@@ -48,27 +46,28 @@ class PS5_Cam():
 
         return video_l, video_r
 
+    def set_wb(self, temp=5200):
+        self.video_capture.set(cv.CAP_PROP_WB_TEMPERATURE, temp)
 
     def release(self):
         self.video_capture.release()
 
-def main():
-    ps_cam = PS5_Cam(3)
 
+def main():
+    ps_cam = PS5_Cam(2)
+    cv.namedWindow("Left")
+    # cv.namedWindow('Right')
+    cv.createTrackbar("balance", "Left", 2_800, 11_000, lambda x: x)
+    # cv.createTrackbar('balance', "Right", 2800, 3700, lambda x: x)
     while True:
         l, r = ps_cam.read_color()
         cv.imshow("Left", l)
         cv.imshow("Right", r)
 
-        # stereo = cv.StereoBM_create(numDisparities=16, blockSize=5)
-        # disparity = stereo.compute(video_l, video_r)
-        # plt.imshow(disparity)
-        # plt.imshow(video_l)
-        # plt.colorbar()
-        # plt.show() # test
+        ps_cam.set_wb(cv.getTrackbarPos("balance", "Left"))
 
         # # cv.imshow("Right", video_r)
-        if cv.waitKey(1) & 0xFF == ord('q'):
+        if cv.waitKey(1) & 0xFF == ord("q"):
             break
 
     # After the loop release the cap object
@@ -78,5 +77,5 @@ def main():
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(main())
