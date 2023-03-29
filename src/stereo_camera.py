0f30ff1b195438127f63ecd71c70147f4c6f1347
cosheimil <a.varvus@ya.ru>
2023-03-29 09:21:25 +0300

    save images directly to repo with calibration camera

--- src/stereo_camera.py
+++ src/stereo_camera.py
@@ -53,21 +53,65 @@ class PS5_Cam:
         self.video_capture.release()
 
 
+start_ptr = 0
+
+
 def main():
-    ps_cam = PS5_Cam(2)
+    ps_cam = PS5_Cam(0)
     cv.namedWindow("Left")
     # cv.namedWindow('Right')
     cv.createTrackbar("balance", "Left", 2_800, 11_000, lambda x: x)
     # cv.createTrackbar('balance', "Right", 2800, 3700, lambda x: x)
+    c = 0
+
+    import shutil
+    from pathlib import Path
+
     while True:
-        l, r = ps_cam.read_color()
+        l, r = ps_cam.read_gray()
         cv.imshow("Left", l)
         cv.imshow("Right", r)
 
-        ps_cam.set_wb(cv.getTrackbarPos("balance", "Left"))
+        if start_ptr == 0 and c == 0:
+            shutil.rmtree(
+                Path(
+                    Path(__file__).parent.parent.parent
+                    / f"./jacinto_ros_perception/img/imageL"
+                )
+            )
+            shutil.rmtree(
+                Path(
+                    Path(__file__).parent.parent.parent
+                    / f"./jacinto_ros_perception/img/imageR"
+                )
+            )
+            Path(
+                Path(__file__).parent.parent.parent
+                / f"./jacinto_ros_perception/img/imageL"
+            ).mkdir()
+            Path(
+                Path(__file__).parent.parent.parent
+                / f"./jacinto_ros_perception/img/imageR"
+            ).mkdir()
+            c += 1
 
-        # # cv.imshow("Right", video_r)
-        if cv.waitKey(1) & 0xFF == ord("q"):
+        if cv.waitKey(33) == ord("a"):
+            cv.imwrite(
+                str(
+                    Path(__file__).parent.parent.parent
+                    / f"./jacinto_ros_perception/img/imageL/Left{c + start_ptr}.png"
+                ),
+                l,
+            )
+            cv.imwrite(
+                str(
+                    Path(__file__).parent.parent.parent
+                    / f"./jacinto_ros_perception/img/imageR/Right{c + start_ptr}.png"
+                ),
+                r,
+            )
+            c += 1
+        if cv.waitKey(27) == ord("q"):
             break
 
     # After the loop release the cap object
