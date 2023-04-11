319ba3bf586740f541996a68190b87594ccc510d
cosheimil <a.varvus@ya.ru>
2023-04-11 20:03:56 +0300

    custom save photo

--- save_photos.py
+++ save_photos.py
@@ -6,9 +6,12 @@ from stereo_vision.camera import *
 
 pattern = (5, 7)
 
+
 def main():
     print(PS5CameraModes.default.value)
-    camera = CameraChessboardSaver(2, PS5CameraModes.dci_2k.value, "ps5_calib_2", pattern[0], pattern[1])
+    camera = CameraChessboardSaver(
+        2, PS5CameraModes.dci_2k.value, "ps5_calib_2", pattern[0], pattern[1]
+    )
     # camera = StereoCam(2, PS5CameraModes.default.value)
     camera.show_videos()
 
