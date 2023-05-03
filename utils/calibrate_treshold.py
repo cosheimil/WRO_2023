92459a36135fe6037accc17693628e48341a752f
Cosheimil <avarvus99@gmail.com>
2023-05-03 11:16:44 +0300

    UI util to tune contrast of image

--- utils/calibrate_treshold.py
+++ utils/calibrate_treshold.py
@@ -1,11 +1,19 @@
 import cv2 as cv
 from PIL import ImageEnhance, Image
+import numpy as np
 
+factor = 1
+
+def nah(x):
+    pass
 
 video = cv.VideoCapture(0)
+cv.namedWindow('Video')
+cv.createTrackbar('Contrast', 'Video', factor, 999, nah)
 ret, frame = video.read()
-im = Image.fromarray(frame)
-enh = ImageEnhance.Contrast(im)
-cv.imshow("View", frame)
-enh.enhance(1.1).show("%10 more contrast")
-cv.waitKey(0)
+while True:
+    if cv.waitKey(1) == ord('a'):
+        break
+    ret, frame = video.read()
+    print(type(frame))
+    
